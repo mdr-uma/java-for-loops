129547841d4996c6fb7fc76b44faef902735ccf0
github username <um_2003_8@hotmail.com>
2022-06-17 19:10:59 -0400

    add outputs of for loop for reference

--- java-for-loops/src/App.java
+++ java-for-loops/src/App.java
@@ -4,7 +4,20 @@ public class App {
 	public static void main(String[] args) {
 		for(int i=0; i<10; i++) {
 			System.out.println("The output is:" + i);
+			//System.out.printf("The output is: %d\n", i); is equivalent to above code.		
 		}
 	}
 
 }
+
+//outputs
+//The output is:0
+//The output is:1
+//The output is:2
+//The output is:3
+//The output is:4
+//The output is:5
+//The output is:6
+//The output is:7
+//The output is:8
+//The output is:9
