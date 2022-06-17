28ce2f351b134a0e3048028f55bda2c515fb88ce
github username <um_2003_8@hotmail.com>
2022-06-17 19:09:06 -0400

    create a for loop that prints a number until it meets the condition

--- java-for-loops/src/App.java
+++ java-for-loops/src/App.java
@@ -2,8 +2,9 @@
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i=0; i<10; i++) {
+			System.out.println("The output is:" + i);
+		}
 	}
 
 }
